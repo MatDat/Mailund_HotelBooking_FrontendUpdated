2fa9d72c6822943eee91fa778a0c03126422ec1d
MatDat <math03t7@stud.kea.dk>
2024-01-11 22:02:21 +0100

    fetch hotel list done

--- .idea/js/index.js
+++ .idea/js/index.js
@@ -1,3 +1,11 @@
+fetch("header.html")
+    .then(respons => respons.text())
+    .then(data => {
+        document.getElementById("headerContent").innerHTML = data;
+    })
+    .catch(error => {
+        console.log('Fejl ved indlæsning af header:', error);
+    })
 
 function addHotel() {
     event.preventDefault()
@@ -18,7 +26,7 @@ function addHotel() {
     let body = JSON.stringify(hotel)
     console.log(body)
 
-    fetch("http://localhost:8080/addHotel", {
+    fetch("http://localhost:3333/addHotel", {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: body
@@ -29,7 +37,54 @@ function addHotel() {
         return response.json();
     }).then(data => {
         console.log("Hotel created succesfully");
+        alert("Hotel was created successfully")
     }).catch(error => {
         console.error("Error when creating hotel " + error);
     });
+}
+
+let hotels = []
+async function fetchHotelList() {
+    try {
+        const response = await fetch("http://localhost:3333/getAllHotels")
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data')
+        }
+
+        hotels = await response.json()
+        console.log(hotels)
+
+        let tableBody = document.getElementById("tableBodyListAllHotels")
+        tableBody.innerHTML = ""
+
+        for (let i = 0; i < hotels.length; i++) {
+            let rowData = document.createElement("tr")
+
+            let hotelIdCell = document.createElement("td")
+            hotelIdCell.textContent = hotels[i].hotelId
+            rowData.appendChild(hotelIdCell)
+
+            let nameCell = document.createElement("td")
+            nameCell.textContent = hotels[i].name
+            rowData.appendChild(nameCell)
+
+            let streetCell = document.createElement("td")
+            streetCell.textContent = hotels[i].street
+            rowData.appendChild(streetCell)
+
+            let cityCell = document.createElement("td")
+            cityCell.textContent = hotels[i].city
+            rowData.appendChild(cityCell)
+
+            let countryCell = document.createElement("td")
+            countryCell.textContent = hotels[i].country
+            rowData.appendChild(countryCell)
+
+            tableBody.appendChild(rowData)
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
 }
